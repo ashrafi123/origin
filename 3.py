13acd68a14c1c0ea69e630ad082cb62079497a8e
ashrafi123 <52068074+ashrafi123@users.noreply.github.com>
2023-03-12 18:02:04 +0330

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,6 +1,6 @@
 list1=['ali','saeed','reza','hamid','saeede','vahid','pedram','mohammad']
 list2=['apple','kiwi']
 list3=['chair' ,'desk']
-list4=['red','yellow','green']
+list4=['red','yellow','green','blue']
 print(list1)
 
